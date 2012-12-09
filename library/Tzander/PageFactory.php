ea22bbfcb4f2e6bfbe6ce06f37be78b35494d95a
Torsten Zander <info@tzander.de>
2012-12-10 00:48:44 +0100

    phpcs fixes

--- library/Tzander/PageFactory.php
+++ library/Tzander/PageFactory.php
@@ -1,5 +1,6 @@
 <?php
 namespace Tzander;
+
 /*
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- library/Tzander/PageFactory/Handler/LocatingElementHandler.php
+++ library/Tzander/PageFactory/Handler/LocatingElementHandler.php
@@ -1,5 +1,6 @@
 <?php
 namespace Tzander\PageFactory\Handler;
+
 /*
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
