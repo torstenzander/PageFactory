4d5630e942a65ae0cde45d5f2c8a2368015e903a
Torsten Zander <info@tzander.de>
2013-01-07 11:18:58 +0100

    Autoloader moved

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__)."/../SplClassLoader.php";
+require_once dirname(__FILE__) . "/../library/SplClassLoader.php";
 
 $classLoader = new SplClassLoader('Tzander', dirname(__FILE__).'/../library');
 $classLoader->register();
